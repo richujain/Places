3e7b20213871d1297a231e79098e905275fc3dc4
richujain <richujain1995@gmail.com>
2022-10-28 19:05:18 -0400

    Custom Form Added

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,10 +1,45 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Colors } from "../../contants/colors";
 
 export default function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
