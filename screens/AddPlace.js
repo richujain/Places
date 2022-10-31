4efe9dc479115fd6a21eb6f4371ae29263af3f2a
richujain <richujain1995@gmail.com>
2022-10-30 21:27:49 -0400

    SQLite installed and using it to manage all the places. Reading and writing functions implemented.

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import PlaceForm from "../components/places/PlaceForm";
+import { insertPlace } from "../util/database";
 
 export default function AddPlace({ navigation }) {
-  function createPlaceHandler(place) {
-    navigation.navigate("AllPlaces", {
-      place: place,
-    });
+  async function createPlaceHandler(place) {
+    await insertPlace(place);
+    navigation.navigate("AllPlaces");
   }
   return <PlaceForm onCreatePlace={createPlaceHandler} />;
 }
--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,14 +1,37 @@
 import { useIsFocused } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import PlacesList from "../components/places/PlacesList";
+import { fetchPlaces } from "../util/database";
 
 export default function AllPlaces({ route }) {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   const isFocused = useIsFocused();
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
     }
-  }, [isFocused, route]);
+
+    if (isFocused) {
+      loadPlaces();
+      // setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused]);
   return <PlacesList places={loadedPlaces} />;
 }
+
+// import { useIsFocused } from "@react-navigation/native";
+// import React, { useEffect, useState } from "react";
+// import PlacesList from "../components/places/PlacesList";
+
+// export default function AllPlaces({ route }) {
+//   const [loadedPlaces, setLoadedPlaces] = useState([]);
+//   const isFocused = useIsFocused();
+//   useEffect(() => {
+
+//     if (isFocused && route.params) {
+//       setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+//     }
+//   }, [isFocused, route]);
+//   return <PlacesList places={loadedPlaces} />;
+// }
